62e25f4e0b14e3ab9f587dd031a30f349e78ea3a
Massimo Giaccone <giacconemassimogmail.com>
2025-07-10 12:35:55 +0200

    Delete debug files

--- ciao.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM 'users'")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
